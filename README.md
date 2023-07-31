5c121504b1bae672358f35fff72ffefb34258334
QwertyR0 <qwerty.rcode@gmail.com>
2023-07-31 18:11:10 +0300

    qwe

--- README.md
+++ README.md
@@ -1,50 +1,59 @@
 # **ezrandom**
-**ezrandom** is a simple random number generator library.
+**ezrandom** is a simple pseudo random number generator library.
 
-![node](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white
-)
+![node](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)
 
 ## 📦**Installation**
-You can use [npm](https://www.npmjs.com) to install:
 ```bash
+npm i @qwertyr0/ezrandom
+# or:
 npm i git+https://github.com/QwertyR0/ezrandom.git
 ```
 
 ## 👩‍💻**USAGE:**
 ```js
-const ezRandom = require('ezrandom');
+import ezRandom from "@qwertyr0/ezrandom";
 
-//random integer:
+//random numbers:
 ezRandom.genInt(1, 3);
-//-> 2
+//-> 1
+
 ezRandom.genInt(1, 3, 4);
 //-> [1, 3, 2, 3]
-ezRandom.genInt(1, 3, 4, {useRandomorg: true});
-//-> [3, 3 ,1, 2]
-// fetches from https://random.org/integers/
 
-//random selection:
+ezRandom.genFloat(1, 3);
+//-> 2.345
+
+// random selection:
 ezRandom.selection(["a", "b"], 1);
 //-> ["a"]
 
-// possibility:
-ezRandom.possibility(1, 10, 3, 4);
+// probability:
+ezRandom.probability(1, 10, 3, 4);
 //-> 1.39
 
-//random password:
+// random password:
 ezRandom.password({
     passLength: 11
 });
 //-> naxohnjykom
-// I will Add More Options To Password Generation!
 
-//random rgb color value:
+// Array shuffle:
+ezRandom.shuffle([12, 53, 44, 23]);
+//-> [44, 12, 23, 53]
+
+// random rgb color value:
 ezRandom.ranColor.rgb();
-//-> [ 52, 7, 109 ]
+//-> [52, 7, 109]
 
-//random hex color value:
+// random hex color value:
 ezRandom.ranColor.hex();
-//-> #a97b30
+//-> "#a97b30"
+
+/*
+ There are more options that are not shown here
+ but it's documented with jsdoc in "main.js"
+*/
 ```
 ## 👔**Maintainer**
 >![QwertyR0](https://github.com/QwertyR0.png?size=100)
@@ -53,14 +62,7 @@ ezRandom.ranColor.hex();
     
 ---
 
-## 🚧**Todo:**
-- Add more support for https://random.org/
-- Make .password() better
-
 ## 🐛**Bugs:**
 Create an issue [here](https://github.com/QwertyR0/ezrandom/issues)
 
-## 📱**Contact:**
-Discord: Qwerty.R#9850
-
-# LICENSE: [MIT](https://choosealicense.com/licenses/mit/)
+# LICENSE: [MIT](./LICENSE)
--- main.js
+++ main.js
@@ -1,66 +1,79 @@
 // Ezrandom
-const axios = require('axios');
-
-/* TODO:
-    - Add times to password
-    - Fix bugs
+/**
+    * @author: QwertyR0
+    * @version: 2.1.0
 */
 
-module.exports = {
+const willExport = {
     
     /**
-        * @param {number} min
-        * @param {number} max
-        * @param {number} time
-        * @param {object} options
+        * @param {number} [min] default: 0
+        * @param {number} [max] default: "Number.MAX_SAFE_INTEGER"
+        * @param {number} [times] default: 
+        * @returns {number}
     */
-
-    genInt: async(min, max, time, options) => {
-        time = (time == null || time == undefined) ? 1 : time ;
-        options = (options == null || options == undefined) ? {useRandomorg: false} : options; 
-        if(typeof(min) !== 'number') throw new ReferenceError("\"min\" Must be a Number!");
-        if(typeof(max) !== 'number') throw new ReferenceError("\"max\" Must be a Number!");
-        if(options.useRandomorg){
-            let response;
-                await axios.get(`https://www.random.org/integers/?num=${time}&min=${min}&max=${max}&col=1&base=10&format=plain&rnd=new`).then(async (res) => {
-                    const data = await res.data;
-                    if(time > 1){
-                        response = data.split("\n");
-                        response.pop();
-                        response.forEach((element, index) => {
-                            response[index] = parseInt(element)
-                        });
-                    } else if(time === 1){
-                        response = data;
-                    }
-                }).catch(error => {
-                    console.error(error)
-                });
-            return response;
-        } else {
-            if(time > 1){
-                var numbers = [];
-                for(var i = 0; i !== time; i++){
-                    numbers.push(Math.floor(Math.random() * max) + min);
-                }
-                return numbers;
-            } else {
-                return Math.floor(Math.random() * max) + min;
+    genInt: (min, max, times) => {
+        times = times || 1;
+        if (min == undefined){
+            min = 0;
+            max = Number.MAX_SAFE_INTEGER;
+        } else if(min && max === undefined){
+            max = min;
+            min = 0;
+        }
+        
+        if(times > 1){
+            let numbers = [];
+            for(let i = 0; i !== times; i++){
+                numbers.push(Math.floor(Math.random() * max) + min);
             }
+
+            return numbers;
+        } else {
+            return Math.floor(Math.random() * max) + min;
         }
     },
 
     /**
-        * @param {array} array
-        * @param {number} amount
+        * @param {float|number} [min] default: 0
+        * @param {float|number} [max] default: "Number.MAX_SAFE_INTEGER"
+        * @param {number} [times] default: 1
+        * @returns {float|number}
     */
+    genFloat: (min, max, times) => {
+        times = times || 1;
+        if (min == undefined){
+            min = 0;
+            max = Number.MAX_SAFE_INTEGER;
+        } else if(min && max === undefined){
+            max = min;
+            min = 0;
+        }
+        
+        if(times > 1){
+            let numbers = [];
+            for(let i = 0; i !== times; i++){
+                numbers.push(Math.random() * (max - min) + min);
+            }
 
+            return numbers;
+        } else {
+            return Math.random() * (max - min) + min;
+        }
+    },
+
+    /**
+        * @param {Array} array
+        * @param {number} amount
+        * @returns {Array}
+    */
     selection: (array, amount) => {
         if(array.constructor !== Array) throw new ReferenceError("Argument must be an Array");
         if(typeof(amount) !== 'number') throw new ReferenceError("\"amount\" Must be a Number!");
-        var selections = [];
+
+        let selections = [];
         amount--;
-        for (var i = 0; i <= amount; i++){
+        for (let i = 0; i <= amount; i++){
             selections.push(array[Math.floor(Math.random() * array.length) + 0]);
         }
         return selections;
@@ -69,61 +82,122 @@ module.exports = {
     /**
         * @param {number} min
         * @param {number} max
-        * @param {number} number
         * @param {number} times
+        * @returns {float|number}
     */
-
-    possibility: (min, max, number, times) => {
+    probability: (min, max, times) => {
         if(typeof(min) !== 'number') throw new ReferenceError("\"min\" Must be a Number!");
         if(typeof(max) !== 'number') throw new ReferenceError("\"max\" Must be a Number!");
-        if(typeof(number) !== 'number' && typeof(number) !== 'object' ) throw new ReferenceError("\"number\" Must be a Number or a number Array!");
         if(typeof(times) !== 'number') throw new ReferenceError("\"times\" Must be a Number!");
-        var randomP = 0;
-        if (typeof(number) === 'object'){
-            for(var i = 0; i <= times; i++){
-                if( number.includes(Math.floor(Math.random() * max) + min) ) {
-                    randomP += 1;
-                }
-            }
-        } else {
-            for(var i = 0; i <= times; i++){
-                if(Math.floor(Math.random() * max) + min === number) randomP += 1;
-            }
+        
+        let randomP = 0;
+        
+        for(let i = 0; i <= times; i++){
+            if(Math.floor(Math.random() * max) + min === 5) randomP += 1;
         }
-        return randomP / times
+        
+        return randomP / times;
     },
 
     /** 
-     * @param {object} options
+     * @param {Object} [options]
+     * @param {number} [pasLength] default: 10 
+     * @param {boolean} [symbols] default: false
+     * @param {boolean} [numbers] default: false
+     * @param {boolean} [uppercase] default: false
+     * @param {boolean} [lowercase] default: true
+     * 
+     * @returns {string}
     */
     password: (options) => {
         if(options.constructor !== Object) throw new ReferenceError("The 1st Argument must be a \"object\"");
-        if(typeof(options.passLength) !== "number") throw new ReferenceError("pasLength Argument must be a \"number\"");
-    options = {
-        passLength: options.passLength || 10,
-    }
+        if(typeof(options.passLength) !== "number" && options.uppercase !== undefined) throw new ReferenceError("pasLength option must be a \"number\"");
+        if(typeof(options.symbols) !== "boolean" && options.uppercase !== undefined) throw new ReferenceError("symbols option must be a \"boolean\"");
+        if(typeof(options.numbers) !== "boolean" && options.uppercase !== undefined) throw new ReferenceError("numbers option must be a \"boolean\"");
+        if(typeof(options.uppercase) !== "boolean" && options.uppercase !== undefined) throw new ReferenceError("uppercase option must be a \"boolean\"");
+        if(typeof(options.lowercase) !== "boolean" && options.uppercase !== undefined) throw new ReferenceError("uppercase option must be a \"boolean\"");
+        
+        options = {
+            passLength: options.passLength || 10,
+            symbols: options.symbols || false,
+            numbers: options.numbers || false,
+            uppercase: options.uppercase || false,
+            lowercase : options.lowercase !== false ? true : false
+        }
 
-    var abc = "abcdefghijklmnopqrstuvwxyz";
-    /*
-    var numbers = "0123456789";
-    var symbols = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
-    */
-    var result = "";
-    for(var i = 0; i <= options.passLength; i++){
-        //let randomPut = Math.floor(Math.random() * 4) + 1;
-        //if(randomPut === 1){
-            result += abc.charAt(Math.floor(Math.random() * abc.length) + 0);
-        //}
-    }
-    return result
+        let list = "";
+
+        let abc = "abcdefghijklmnopqrstuvwxyz";
+        let numbers = "0123456789";
+        let symbols = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+
+        if (options.symbols){
+            list += symbols;
+        }
+
+        if (options.uppercase){
+            list += abc.toUpperCase();
+        }
+
+        if (options.numbers){
+            list += numbers;
+        }
+
+        if (options.lowercase){
+            list += abc;
+        }
+
+        if(list == 0) return false;
+
+        let result = "";
+        for (let i = 0; i <= options.passLength; i++){
+                result += abc.charAt(Math.floor(Math.random() * list.length));
+        }
+
+        return result;
     },
+
     ranColor: {
+        /**
+         * @returns {string}
+         */
         hex: () => {
-            return "#" + (Math.floor(Math.random() * 16777215) + 0).toString(16);
+            return "#" + (Math.floor(Math.random() * 16777215)).toString(16);
         },
+
+        /**
+         * @returns {Array}
+         */
         rgb: () => {
-            let result = [(Math.floor(Math.random() * 255) + 0), Math.floor(Math.random() * 255) + 0, Math.floor(Math.random() * 255) + 0];
+            let result = [(Math.floor(Math.random() * 255)), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
             return result;
         }
+    },
+    /** 
+     * @param {Array} arr
+     * @returns {Array}
+    */
+    shuffle: (arr) => {
+        return arr.sort(() => 0.5 - Math.random());
+    },
+
+    /** 
+     * @param {number} number
+     * @param {number|float} [range] default: 3.8
+     * 
+     * @returns {number|float}
+    */
+    mutate: (number, range) => {
+        if(typeof(number) !== 'number') throw new ReferenceError("No number to be mutated is passed.");
+        range = range || 3.8;
+
+        let mutation = 0;
+        while(mutation === 0){
+            mutation = Math.random() * (range - (-range)) + -range;
+        }
+
+        return number + mutation
     }
 }
+
+export default willExport;
